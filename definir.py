866e557b34f6383e94f8c2318bd3d6cd6ab043d7
Sabrina Guantay <52327724+sabriguantay@users.noreply.github.com>
2021-06-30 16:25:22 -0300

    Se agrega las funciones

---
+++ definir.py
@@ -0,0 +1,8 @@
+def obtener_energía_ganada(peso):
+    a = 12 + peso
+    return a
+    
+def correr(energy, distancia):
+    velocidad = 5 + (energy / 10) 
+    tiempo = distancia/velocidad 
+    return tiempo
